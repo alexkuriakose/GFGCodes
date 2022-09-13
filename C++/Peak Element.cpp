0c0a8c779f5a12ef59a3594f9fdb35cab1ba51f1
Alex Kuriakose <56859069+alexkuriakose@users.noreply.github.com>
2022-09-13 09:32:29 +0530

    Arrays Searching
    An element is called a peak element if its value is not smaller than the value of its adjacent elements(if they exists).
    Given an array arr[] of size N, Return the index of any one of its peak elements.
    Note: The generated output will always be 1 if the index that you return is correct. Otherwise output will be 0. 
    
    Example:
    
    Input: 
    N = 3
    arr[] = {1,2,3}
    Possible Answer: 2
    Generated Output: 1
    Explanation: index 2 is 3.
    It is the peak element as it is 
    greater than its neighbour 2.
    If 2 is returned then the generated output will be 1 else 0.

---
+++ C++/Peak Element.cpp
@@ -0,0 +1,59 @@
+#include<stdio.h>
+#include<stdbool.h>
+
+int peakElement(int arr[], int n)
+{
+   if (n == 1)
+        return 0;
+    if (arr[0] >= arr[1])
+        return 0;
+    if (arr[n - 1] >= arr[n - 2])
+        return n - 1;
+  
+    // check for every other element
+    for (int i = 1; i < n - 1; i++) {
+  
+        // check if the neighbors are smaller
+        if (arr[i] >= arr[i - 1] && arr[i] >= arr[i + 1])
+            return i;
+    }
+}
+
+int main() {
+	int t;
+	scanf("%d", &t);
+	while(t--)
+	{
+		int n;
+		scanf("%d", &n);
+		int a[n], tmp[n];
+		for(int i=0;i<n;i++)
+		{
+			scanf("%d", &a[i]);
+			tmp[i] = a[i];
+		}
+		bool f=0;
+		
+		int A = peakElement(tmp,n);
+		
+		if(A<0 && A>=n)
+		    printf("0\n");
+		else
+		{
+    		if(n==1 && A==0)
+    		    f=1;
+    		else if(A==0 && a[0]>=a[1])
+    		    f=1;
+    		else if(A==n-1 && a[n-1]>=a[n-2])
+    		    f=1;
+    		else if(a[A]>=a[A+1] && a[A]>= a[A-1])
+    		    f=1;
+    		else
+    		    f=0;
+    		printf("%d\n", f);
+		}
+		
+	}
+
+	return 0;
+}
